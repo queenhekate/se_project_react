4248fd6461e93856ad1dc42fea9d0b65340e16af
JOHANNA SCHNELL <JOHANNASCHNELL@GMAIL.COM>
2024-10-13 13:54:15 -0700

    add weathercard code

--- src/components/App/WeatherCard/WeatherCard.jsx
+++ src/components/App/WeatherCard/WeatherCard.jsx
@@ -1,11 +1,25 @@
 import "./WeatherCard.css";
-import sunny from "../../../images/Sunny.png";
+import { weatherOptions } from "../../../utils/constants";
 
 function WeatherCard({ weatherData }) {
+  const filteredOptions = weatherOptions.filter((option) => {
+    return (
+      option.day === weatherData.isDay &&
+      option.condition === weatherData.condition
+    );
+  });
+
+  const weatherOptionUrl = filteredOptions[0]?.url;
+  const weatherOptionCondition = filteredOptions[0]?.condition;
+
   return (
     <section className="weather-card">
       <p className="weather-card__temp">{weatherData.temp.F}&deg;F </p>
-      <img src={sunny} alt="" className="weather-card__image" />
+      <img
+        src={weatherOptionUrl}
+        alt={`${weatherOptionCondition}`}
+        className="weather-card__image"
+      />
     </section>
   );
 }
