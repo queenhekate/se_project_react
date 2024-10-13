c1704f0897b13a0bfde60caf6cceae920287140a
JOHANNA SCHNELL <JOHANNASCHNELL@GMAIL.COM>
2024-10-13 13:18:09 -0700

    weather options constants add

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,77 @@
+export const weatherOptions = [
+  {
+    day: true,
+    condition: "clear",
+    url: new URL("../images/Day/Clear.png", import.meta.url).href,
+  },
+
+  {
+    day: true,
+    condition: "cloudy",
+    url: new URL("../images/Day/Cloudy.png", import.meta.url).href,
+  },
+
+  {
+    day: true,
+    condition: "fog",
+    url: new URL("../images/Day/Fog.png", import.meta.url).href,
+  },
+
+  {
+    day: true,
+    condition: "rain",
+    url: new URL("../images/Day/Rain.png", import.meta.url).href,
+  },
+
+  {
+    day: true,
+    condition: "snow",
+    url: new URL("../images/Day/Snow.png", import.meta.url).href,
+  },
+
+  {
+    day: true,
+    condition: "storm",
+    url: new URL("../images/Day/Storm.png", import.meta.url).href,
+  },
+
+  {
+    day: false,
+    condition: "clear",
+    url: new URL("../images/Night/Clear.png", import.meta.url).href,
+  },
+
+  {
+    day: false,
+    condition: "cloudy",
+    url: new URL("../images/Night/Cloudy.png", import.meta.url).href,
+  },
+
+  {
+    day: false,
+    condition: "fog",
+    url: new URL("../images/Night/Fog.png", import.meta.url).href,
+  },
+
+  {
+    day: false,
+    condition: "rain",
+    url: new URL("../images/Night/Rain.png", import.meta.url).href,
+  },
+
+  {
+    day: false,
+    condition: "snow",
+    url: new URL("../images/Night/Snow.png", import.meta.url).href,
+  },
+
+  {
+    day: false,
+    condition: "storm",
+    url: new URL("../images/Night/Storm.png", import.meta.url).href,
+  },
+];
+
 export const defaultClothingItems = [
   {
     _id: 0,
