5c7c56eb9c34e6c15fd66ee0a4b2a973480902f7
JOHANNA SCHNELL <JOHANNASCHNELL@GMAIL.COM>
2024-10-12 20:37:39 -0700

    handlers

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,22 +7,27 @@ import ModalWithForm from "./ModalWithForm/ModalWithForm";
 
 function App() {
   const [weatherData, setWeatherData] = useState({ type: "hot" });
-  const [activeModal, setActiveModal] = useState("add-garment");
+  const [activeModal, setActiveModal] = useState("");
 
   const handleAddClick = () => {
     setActiveModal("add-garment");
   };
 
+  const closeActiveModal = () => {
+    setActiveModal("");
+  };
+
   return (
     <div className="page">
       <div className="page__content">
-        <Header handleAddClick={setActiveModal} />
+        <Header handleAddClick={handleAddClick} />
         <Main weatherData={weatherData} />
       </div>
       <ModalWithForm
         title="New Garment"
         buttonText="Add garment"
         activeModal={activeModal}
+        handleCloseClick={closeActiveModal}
       >
         <label htmlFor="name" className="modal__label">
           Name{" "}
--- src/components/App/ModalWithForm/ModalWithForm.jsx
+++ src/components/App/ModalWithForm/ModalWithForm.jsx
@@ -1,11 +1,21 @@
 import "./ModalWithForm.css";
 
-function ModalWithForm({ children, buttonText, title, activeModal }) {
+function ModalWithForm({
+  children,
+  buttonText,
+  title,
+  activeModal,
+  handleCloseClick,
+}) {
   return (
     <div className={`modal ${activeModal === "add-garment" && "modal_opened"}`}>
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
-        <button type="button" className="modal__close"></button>
+        <button
+          onClick={handleCloseClick}
+          type="button"
+          className="modal__close"
+        ></button>
         <form className="modal__form">
           {children}
           <button type="submit" className="modal__submit">
