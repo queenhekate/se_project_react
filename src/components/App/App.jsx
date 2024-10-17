fd99d27e0e3f918d0511365c429b8433a24934d1
JOHANNA SCHNELL <JOHANNASCHNELL@GMAIL.COM>
2024-10-16 19:49:49 -0700

    test

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -73,21 +73,36 @@ function App() {
         <fieldset className="modal__radio-buttons">
           <legend className="modal__legend">Select the weather type:</legend>
           <label htmlFor="hot" className="modal__label modal__label_type_radio">
-            <input id="hot" type="radio" className="modal__radio-input" />
+            <input
+              id="hot"
+              type="radio"
+              name="radAnswer"
+              className="modal__radio-input"
+            />
             &nbsp;Hot
           </label>
           <label
             htmlFor="warm"
             className="modal__label modal__label_type_radio"
           >
-            <input id="warm" type="radio" className="modal__radio-input" />
+            <input
+              id="warm"
+              type="radio"
+              name="radAnswer"
+              className="modal__radio-input"
+            />
             &nbsp;Warm
           </label>
           <label
             htmlFor="cold"
             className="modal__label modal__label_type_radio"
           >
-            <input id="cold" type="radio" className="modal__radio-input" />
+            <input
+              id="cold"
+              type="radio"
+              name="radAnswer"
+              className="modal__radio-input"
+            />
             &nbsp;Cold
           </label>
         </fieldset>
