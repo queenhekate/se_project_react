16de76a1b58733fb7105e7e1d6f58ef5da57e88e
Johanna Schnell <johannaschnell@gmail.com>
2025-06-06 12:33:38 -0700

    debug data

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -21,10 +21,6 @@ function WeatherCard({ weatherData }) {
     weatherOption = filteredOptions[0];
   }
 
-  console.log("weatherData.isDay:", weatherData.isDay);
-  console.log("weatherData.condition:", weatherData.condition);
-  console.log("weatherOptions:", weatherOptions);
-
   return (
     <section className="weather-card">
       <p className="weather-card__temp">
